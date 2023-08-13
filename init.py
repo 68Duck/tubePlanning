dd475b7f8f7d8458e0985f001f2d9b1c581539ae
68Duck <Joshuahenry111@yahoo.co.uk>
2023-08-13 19:41:39 +0100

    Added drawing functionality with routes

--- init.py
+++ init.py
@@ -4,6 +4,8 @@ from db_generator import DB_generator
 from flask import g,Flask,render_template,request,make_response
 from os import path
 import sqlite3
+from Solver import Solver
+from db import DB
 
 
 fileDir = path.dirname(__file__)
@@ -25,12 +27,47 @@ def get_db():
     return db
 
 
+@app.route("/drawRequest", methods = ["POST"])
+def drawRequest():
+    data = request.get_json()
+    if data is None:
+        pass
+    else:
+        origin = data["origin"]
+        destination = data["destination"]
+        solver = Solver(DB("distances.db", get_db()))
+        station = solver.findPath(origin, destination)
+        route = []
+        while station.parentStation is not None:
+            route.append(station)
+            station = station.parentStation
 
-@app.route("/requestStation", methods=["POST"])
-def requestNextStation():
-    time.sleep(0.2)
-    dbGenerator = DB_generator("distances.db", "distances.csv", get_db())
-    return dbGenerator.requestNextStation()
+        points = list(map(lambda x: [solver.getStationCoordinates(x.getName())["x"],
+                                   solver.getStationCoordinates(x.getName())["y"],
+                                   x.getLine()], route))
+
+        lines = []
+        print(points)
+        for i in range(len(points) - 1):
+            print(i)
+            print(points[i])
+            print(points[i][0])
+            print(points[i][1])
+            print(points[i+1][0])
+            print(points[i+1][1])
+            print(points[i][2])
+            lines.append([points[i][0], points[i][1],
+                         points[i+1][0], points[i+1][1], points[i][2]])
+
+        return lines
+
+    return "nothing"
+
+# @app.route("/requestStation", methods=["POST"])
+# def requestNextStation():
+#     time.sleep(0.2)
+#     dbGenerator = DB_generator("distances.db", "distances.csv", get_db())
+#     return dbGenerator.requestNextStation()
 
 
 # @app.route("/sendCoordinates", methods = ["POST"])
--- static/app.js
+++ static/app.js
@@ -1,41 +1,80 @@
-
-
-
-
-const requestStation = async () => {
-   const url = '/requestStation'; // the URL to send the HTTP request to
-   const body = JSON.stringify("") // whatever you want to send in the body of the HTTP request
-   const headers = {'Content-Type': 'application/json'}; // if you're sending JSON to the server
-   const method = 'POST';
-   const response = await fetch(url, { method, body, headers });
-   const data = await response.text(); // or response.json() if your server returns JSON
-   console.log(data);
-}
-
-const sendCoordinates = async (x, y) => {
-   const url = '/sendCoordinates'; // the URL to send the HTTP request to
-   const body = JSON.stringify({"x" : x, "y" : y }) // whatever you want to send in the body of the HTTP request
-   const headers = {'Content-Type': 'application/json'}; // if you're sending JSON to the server
-   const method = 'POST';
-   const response = await fetch(url, { method, body, headers });
-   const data = await response.text(); // or response.json() if your server returns JSON
-   console.log(data);
-}
-
-requestStation();
-
 let canvas = document.getElementById('canvas');
 canvas.width = 1200
 canvas.height = 846.6
 context = canvas.getContext('2d');
 var canvasRectangle = canvas.getBoundingClientRect();
 
-canvas.onclick = function(e) {
-    var x = e.x - canvasRectangle.left;
-    var y = e.y - canvasRectangle.top;
-    sendCoordinates(x, y);
-    requestStation();
+function drawLine(x1, y1, x2, y2, line){
+        console.log("Drawing line")
+        console.log(x1, y1, x2, y2, line)
+        context.beginPath();
+//        console.log(x1, y1, x2, y2)
+        context.moveTo(x1 + canvasRectangle.left, y1 + canvasRectangle.top);
+        context.lineTo(x2 + canvasRectangle.left, y2 + canvasRectangle.top);
+        switch (line) {
+            case "Metropolitan":
+                context.strokeStyle = '#97015e';
+                break;
+        }
+        context.lineWidth = 4;
+        context.stroke();
 }
+
+const sendDrawRequest = async (origin, destination) => {
+   const url = '/drawRequest'; // the URL to send the HTTP request to
+   const body = JSON.stringify({"origin" : origin, "destination" : destination }) // whatever you want to send in the body of the HTTP request
+   const headers = {'Content-Type': 'application/json'}; // if you're sending JSON to the server
+   const method = 'POST';
+   const response = await fetch(url, { method, body, headers });
+   const data = await response.text(); // or response.json() if your server returns JSON
+   const obj = JSON.parse(data);
+   console.log(obj);
+   //Should return a list of pairs of coordinates and line
+
+    for (i = 0; i < obj.length; i++) {
+        console.log(obj[i])
+        drawLine(obj[i][0], obj[i][1], obj[i][2], obj[i][3], obj[i][4]);
+    }
+
+}
+
+sendDrawRequest("UPMINSTER", "UPNEY")
+sendDrawRequest("AMERSHAM", "ALDGATE")
+
+//const requestStation = async () => {
+//   const url = '/requestStation'; // the URL to send the HTTP request to
+//   const body = JSON.stringify("") // whatever you want to send in the body of the HTTP request
+//   const headers = {'Content-Type': 'application/json'}; // if you're sending JSON to the server
+//   const method = 'POST';
+//   const response = await fetch(url, { method, body, headers });
+//   const data = await response.text(); // or response.json() if your server returns JSON
+//   console.log(data);
+//}
+//
+//const sendCoordinates = async (x, y) => {
+//   const url = '/sendCoordinates'; // the URL to send the HTTP request to
+//   const body = JSON.stringify({"x" : x, "y" : y }) // whatever you want to send in the body of the HTTP request
+//   const headers = {'Content-Type': 'application/json'}; // if you're sending JSON to the server
+//   const method = 'POST';
+//   const response = await fetch(url, { method, body, headers });
+//   const data = await response.text(); // or response.json() if your server returns JSON
+//   console.log(data);
+//}
+//
+//requestStation();
+//
+//let canvas = document.getElementById('canvas');
+//canvas.width = 1200
+//canvas.height = 846.6
+//context = canvas.getContext('2d');
+//var canvasRectangle = canvas.getBoundingClientRect();
+//
+//canvas.onclick = function(e) {
+//    var x = e.x - canvasRectangle.left;
+//    var y = e.y - canvasRectangle.top;
+//    sendCoordinates(x, y);
+//    requestStation();
+//}
 
 //DRAWING = true;
 //previousX = null
